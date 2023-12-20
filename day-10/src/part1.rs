068ee8928a18996bab8248935069bca0c14a50d3
phlafoo <shuval@ualberta.ca>
2023-12-20 00:03:03 -0700

    day 10 part 1 cleaned up

--- day-10/src/part1.rs
+++ day-10/src/part1.rs
@@ -1,55 +1,12 @@
-
-// use std::collections::HashMap;
-
 use crate::custom_error::AocError;
 use phf::phf_map;
 
-// #[derive(PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
-// enum Direction {
-//     Up,
-//     Down,
-//     Left,
-//     Right,
-// }
+const UP: (i32, i32) = (0, -1);
+const DOWN: (i32, i32) = (0, 1);
+const LEFT: (i32, i32) = (-1, 0);
+const RIGHT: (i32, i32) = (1, 0);
 
-// struct Dir {
-//     x: i32,
-//     y: i32,
-// }
-
-// impl Dir {
-//     const UP   : Dir = Dir { x:  0, y: -1 };
-//     const DOWN : Dir = Dir { x:  0, y:  1 };
-//     const LEFT : Dir = Dir { x: -1, y:  0 };
-//     const RIGHT: Dir = Dir { x:  1, y:  0 };
-// }
-
-
-// impl Direction {
-//     pub fn rev(&self) -> Direction {
-//         match self {
-//             Direction::Up => Direction::Down,
-//             Direction::Down => Direction::Up,
-//             Direction::Left => Direction::Right,
-//             Direction::Right => Direction::Left,
-//         }
-//     }
-
-    // pub fn get_index(&self, row_length: i32) -> usize {
-    //     match dir {
-    //         Direction::Up => index.saturating_sub(row_length),
-    //         Direction::Down => index + row_length,
-    //         Direction::Left => index.saturating_sub(1),
-    //         Direction::Right => index + 1,
-    //     };
-    // }
-    // }
-
-const UP   : (i32, i32) = ( 0, -1);
-const DOWN : (i32, i32) = ( 0,  1);
-const LEFT : (i32, i32) = (-1,  0);
-const RIGHT: (i32, i32) = ( 1,  0);
-
+// Associate pipes with directions
 static PIPE_DIR_MAP: phf::Map<u8, [(i32, i32); 2]> = phf_map! {
     b'|' => [UP,   DOWN],
     b'-' => [LEFT, RIGHT],
@@ -58,116 +15,52 @@ static PIPE_DIR_MAP: phf::Map<u8, [(i32, i32); 2]> = phf_map! {
     b'7' => [LEFT, DOWN],
     b'F' => [DOWN, RIGHT],
 };
-// static PIPE_DIR_MAP: phf::Map<u8, [Direction; 2]> = phf_map! {
-//     b'|' => [Direction::Up, Direction::Down],
-//     b'-' => [Direction::Left, Direction::Right],
-//     b'L' => [Direction::Up, Direction::Right],
-//     b'J' => [Direction::Up, Direction::Left],
-//     b'7' => [Direction::Left, Direction::Down],
-//     b'F' => [Direction::Down, Direction::Right],
-// };
-// static DIR_INDEX_MAP: phf::Map<Direction, (i32, i32)> = phf_map! {
-//     Direction::Up => (0, -1),
-//     Direction::Up => (0, -1),
-// };
 
-#[allow(arithmetic_overflow)]
-pub fn process(
-    input: &str,
-) -> miette::Result<String, AocError> {
+pub fn process(input: &str) -> miette::Result<String, AocError> {
     let row_length = (input.find('\n').unwrap() + 1) as i32;
     let start_index = input.find('S').unwrap() as i32;
+    // indexing is easier if in bytes
     let input = input.as_bytes();
 
+    // Find first direction to go from start
     let mut dir = (0, 0);
     for (x, y) in &[UP, DOWN, LEFT, RIGHT] {
-        let p = input[(start_index + (row_length * y) + x) as usize];
-        if [b'.', b'\n'].contains(&p) {
+        let pipe = input[(start_index + (row_length * y) + x) as usize];
+        if [b'.', b'\n'].contains(&pipe) {
             continue;
         }
-        if PIPE_DIR_MAP[&p].contains(&(-x, -y)) {
+        // If the reverse of current direction is one of the pipe's assoc. directions
+        if PIPE_DIR_MAP[&pipe].contains(&(-x, -y)) {
             dir = (*x, *y);
             break;
         }
-    };
+    }
     let mut index = start_index;
     let mut steps = 0;
     loop {
+        steps += 1;
+
         let (x, y) = dir;
         index = index + (row_length * y) + x;
-        steps += 1;
-        let p = &input[index as usize];
-        if *p == b'S' {
+        let pipe = &input[index as usize];
+
+        if *pipe == b'S' {
             break;
         }
-        let dirs = &PIPE_DIR_MAP[p];
-        
-        dir = if (-x, -y) == dirs[0] {
-            dirs[1]
+        // get assoc. directions
+        let directions = &PIPE_DIR_MAP[pipe];
+
+        // Select the direction that is not the reverse of the current direction
+        dir = if (-x, -y) == directions[0] {
+            directions[1]
         } else {
-            dirs[0]
+            directions[0]
         }
     }
-    // dbg!(steps);
+    // Furthest point will be halfway around loop
     let result = steps / 2;
     Ok(result.to_string())
 }
-// #[allow(arithmetic_overflow)]
-// pub fn process(
-//     input: &str,
-// ) -> miette::Result<String, AocError> {
-//     let row_length = (input.find('\n').unwrap() + 1) as i32;
-//     let start_index = input.find('S').unwrap() as i32;
-//     let input = input.as_bytes();
-
-//     let dir_index_map: HashMap<Direction, (i32, i32)> = HashMap::from([
-//         (Direction::Up, (0, -1)),
-//         (Direction::Down, (0, 1)),
-//         (Direction::Left, (-1, 0)),
-//         (Direction::Right, (1, 0)),
-//     ]);
-//     // let neighbors: [(i32, i32); 4] = [
-//     //     (0, -1),
-//     //     (0, 1),
-//     //     (-1, 0),
-//     //     (1, 0),
-//     // ];
-//     // println!("{}", input[start_index - row_length] as char);
-//     // println!("{}", input[start_index] as char);
-//     // println!("{}", input[start_index + row_length] as char);
-//     let mut dir = &Direction::Up;
-//     for (d, (x, y)) in &dir_index_map {
-//         let p = input[(start_index + (row_length * y) + x) as usize];
-//         if [b'.', b'\n'].contains(&p) {
-//             continue;
-//         }
-//         if PIPE_DIR_MAP[&p].contains(&d.rev()) {
-//             dir = d;
-//             break;
-//         }
-//     };
-//     let mut index = start_index;
-//     let mut steps = 0;
-//     loop {
-//         let (x, y) = dir_index_map[dir];
-//         index = index + (row_length * y) + x;
-//         steps += 1;
-//         let p = &input[index as usize];
-//         if *p == b'S' {
-//             break;
-//         }
-//         let dirs = &PIPE_DIR_MAP[p];
-        
-//         dir = if dir.rev() == dirs[0] {
-//             &dirs[1]
-//         } else {
-//             &dirs[0]
-//         }
-//     }
-//     // dbg!(steps);
-//     let result = steps / 2;
-//     Ok(result.to_string())
-// }
 
 #[cfg(test)]
 mod tests {
